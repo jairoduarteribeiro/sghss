fbfa6bf87c9084a6ec1eead93c8e427257b742d7
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-10-26 19:20:05 +0000

    feat: should not create an Email longer than 254 chars

--- src/domain/constants/validation-messages.ts
+++ src/domain/constants/validation-messages.ts
@@ -2,4 +2,5 @@ export const VALIDATION_MESSAGES = {
   CPF_INVALID_FORMAT: "CPF has an invalid format",
   CPF_ALL_DIGITS_ARE_THE_SAME: "CPF cannot have all digits the same",
   CPF_INVALID_CHECK_DIGITS: "CPF has invalid check digits",
+  EMAIL_TOO_LONG: "Email cannot be longer than 254 characters",
 };
--- src/domain/value-objects/email.ts
+++ src/domain/value-objects/email.ts
@@ -1,6 +1,16 @@
+import { VALIDATION_MESSAGES } from "@/domain/constants/validation-messages";
+import { DomainError } from "@/domain/errors/domain-error";
+
 export class Email {
   constructor(public readonly value: string) {
     this.value = this.normalize(value);
+    this.validate(this.value);
+  }
+
+  private validate(email: string): void {
+    if (email.length > 254) {
+      throw new DomainError(VALIDATION_MESSAGES.EMAIL_TOO_LONG);
+    }
   }
 
   private normalize(email: string): string {
