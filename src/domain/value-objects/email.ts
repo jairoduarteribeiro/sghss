75cad38a059281ed094b310d70da3048ddbf38fd
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-10-26 19:31:10 +0000

    feat: should throw if an invalid Email is provided

--- src/domain/value-objects/email.ts
+++ src/domain/value-objects/email.ts
@@ -1,6 +1,9 @@
 import { VALIDATION_MESSAGES } from "@/domain/constants/validation-messages";
 import { DomainError } from "@/domain/errors/domain-error";
 
+const EMAIL_REGEX =
+  /^[a-z][a-z0-9]+(\.[a-z][a-z0-9]+)*@[a-z][a-z0-9]+(\.[a-z][a-z0-9]+)+$/;
+
 export class Email {
   constructor(public readonly value: string) {
     this.value = this.normalize(value);
@@ -11,9 +14,16 @@ export class Email {
     if (email.length > 254) {
       throw new DomainError(VALIDATION_MESSAGES.EMAIL_TOO_LONG);
     }
+    if (!this.hasValidFormat(email)) {
+      throw new DomainError(VALIDATION_MESSAGES.EMAIL_INVALID_FORMAT);
+    }
   }
 
   private normalize(email: string): string {
     return email.trim().toLowerCase();
+  }
+
+  private hasValidFormat(email: string): boolean {
+    return EMAIL_REGEX.test(email);
   }
 }
