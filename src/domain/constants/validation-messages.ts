b27875d7be173b0d142178cf291678ab8949680a
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-10-26 19:31:10 +0000

    feat: should throw if an invalid email is provided

--- src/domain/constants/validation-messages.ts
+++ src/domain/constants/validation-messages.ts
@@ -3,4 +3,5 @@ export const VALIDATION_MESSAGES = {
   CPF_ALL_DIGITS_ARE_THE_SAME: "CPF cannot have all digits the same",
   CPF_INVALID_CHECK_DIGITS: "CPF has invalid check digits",
   EMAIL_TOO_LONG: "Email cannot be longer than 254 characters",
+  EMAIL_INVALID_FORMAT: "Email has an invalid format",
 };
