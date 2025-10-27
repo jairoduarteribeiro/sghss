8631d3db7af0f597d28d2cd780b5f9fced084bfa
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-10-27 16:15:37 +0000

    test: should not create a Patient with invalid Cpf

--- tests/domain/entities/patient.test.ts
+++ tests/domain/entities/patient.test.ts
@@ -1,4 +1,5 @@
 import { Patient } from "@/domain/entities/patient";
+import { DomainError } from "@/domain/errors/domain-error";
 import { describe, test, expect } from "bun:test";
 
 describe("Patient entity", () => {
@@ -16,5 +17,15 @@ describe("Patient entity", () => {
     expect(patient.cpf.value).toBe(input.cpf);
     expect(patient.password.hash.length).toBeGreaterThan(0);
     expect(patient.password.hash).not.toBe(input.password);
+  });
+
+  test("Should throw a DomainError if an invalid CPF is provided", async () => {
+    const input = {
+      name: "John Doe",
+      cpf: "111.111.111-11",
+      email: "john.doe@example.com",
+      password: "Password123!",
+    };
+    await expect(Patient.create(input)).rejects.toThrow(DomainError);
   });
 });
