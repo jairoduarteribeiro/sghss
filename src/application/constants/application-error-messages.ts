c1618bb2e92f354bee8d8e9d85880c1e316e2191
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-10-28 21:04:18 +0000

    feat: should throw ApplicationError if DomainError occurs

--- src/application/constants/application-error-messages.ts
+++ src/application/constants/application-error-messages.ts
@@ -1,4 +1,5 @@
 export const APPLICATION_ERROR_MESSAGES = {
   EMAIL_ALREADY_IN_USE: "Email is already in use",
   CPF_ALREADY_IN_USE: "CPF is already in use",
+  INVALID_INPUT_DATA: "Invalid input data",
 };
--- src/application/use-cases/register-patient.use-case.ts
+++ src/application/use-cases/register-patient.use-case.ts
@@ -9,6 +9,7 @@ import { Email } from "@/domain/value-objects/email";
 import { Password } from "@/domain/value-objects/password";
 import { ApplicationError } from "@/application/errors/application-error";
 import { APPLICATION_ERROR_MESSAGES } from "@/application/constants/application-error-messages";
+import { DomainError } from "@/domain/errors/domain-error";
 
 export class RegisterPatientUseCase {
   constructor(private readonly patientRepository: PatientRepository) {}
@@ -16,25 +17,36 @@ export class RegisterPatientUseCase {
   public async execute(
     input: RegisterPatientInput
   ): Promise<RegisterPatientOutput> {
-    const cpf = new Cpf(input.cpf);
-    if (await this.patientRepository.findByCpf(cpf)) {
-      throw new ApplicationError(APPLICATION_ERROR_MESSAGES.CPF_ALREADY_IN_USE);
+    try {
+      const cpf = new Cpf(input.cpf);
+      if (await this.patientRepository.findByCpf(cpf)) {
+        throw new ApplicationError(
+          APPLICATION_ERROR_MESSAGES.CPF_ALREADY_IN_USE
+        );
+      }
+      const email = new Email(input.email);
+      if (await this.patientRepository.findByEmail(email)) {
+        throw new ApplicationError(
+          APPLICATION_ERROR_MESSAGES.EMAIL_ALREADY_IN_USE
+        );
+      }
+      const patient = Patient.create({
+        name: input.name,
+        email,
+        cpf,
+        password: await Password.create(input.password),
+      });
+      await this.patientRepository.save(patient);
+      return {
+        id: patient.id,
+      };
+    } catch (error) {
+      if (error instanceof DomainError) {
+        throw new ApplicationError(
+          `${APPLICATION_ERROR_MESSAGES.INVALID_INPUT_DATA}: ${error.message}`
+        );
+      }
+      throw error;
     }
-    const email = new Email(input.email);
-    if (await this.patientRepository.findByEmail(email)) {
-      throw new ApplicationError(
-        APPLICATION_ERROR_MESSAGES.EMAIL_ALREADY_IN_USE
-      );
-    }
-    const patient = Patient.create({
-      name: input.name,
-      email,
-      cpf,
-      password: await Password.create(input.password),
-    });
-    await this.patientRepository.save(patient);
-    return {
-      id: patient.id,
-    };
   }
 }
