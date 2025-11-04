e5db715d0f1129eb0611033b57242122a7d7abcb
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-11-04 11:05:52 +0000

    feat: should create a Name with valid length

--- src/domain/value-objects/name.ts
+++ src/domain/value-objects/name.ts
@@ -1,12 +1,28 @@
+const MIN_LENGTH = 2;
+const MAX_LENGTH = 255;
+
 export class Name {
   private constructor(public readonly value: string) {}
 
   static from(name: string): Name {
     const normalizedName = this.normalize(name);
+    this.validate(normalizedName);
     return new Name(normalizedName);
   }
 
   private static normalize(name: string): string {
     return name.trim().replace(/\s+/g, " ");
+  }
+
+  private static validate(name: string): void {
+    if (!this.isValidLength(name)) {
+      throw new Error(
+        `Name length must be between ${MIN_LENGTH} and ${MAX_LENGTH} characters`
+      );
+    }
+  }
+
+  private static isValidLength(name: string): boolean {
+    return MIN_LENGTH <= name.length && name.length <= MAX_LENGTH;
   }
 }
