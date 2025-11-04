087b7727e52bc0ee322d68b1909ae6cc275a123a
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-11-04 10:57:33 +0000

    feat: should create a trimmed and normalized name

--- src/domain/value-objects/name.ts
+++ src/domain/value-objects/name.ts
@@ -1,7 +1,12 @@
 export class Name {
   private constructor(public readonly value: string) {}
 
-  static from(value: string): Name {
-    return new Name(value);
+  static from(name: string): Name {
+    const normalizedName = this.normalize(name);
+    return new Name(normalizedName);
+  }
+
+  private static normalize(name: string): string {
+    return name.trim().replace(/\s+/g, " ");
   }
 }
