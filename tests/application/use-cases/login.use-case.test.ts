d39f0a65dfd4fc75032aa5bbcb7cbb36310b0d7c
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-11-06 16:46:23 +0000

    test: should login with valid credentials

---
+++ tests/application/use-cases/login.use-case.test.ts
@@ -0,0 +1,54 @@
+import type { LoginUseCase } from "@/application/use-cases/login.use-case";
+import { User } from "@/domain/entities/user";
+import { Email } from "@/domain/value-objects/email";
+import { Password } from "@/domain/value-objects/password";
+import type { InMemoryUserRepository } from "@/infrastructure/persistence/in-memory/in-memory-user.repository";
+import { SYMBOLS } from "@/inversify.symbols";
+import { testContainer } from "@tests/config/inversify.container";
+import {
+  describe,
+  test,
+  expect,
+  beforeEach,
+  beforeAll,
+  afterEach,
+} from "bun:test";
+
+const JWT_REGEX = /^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/;
+
+describe("Login Use Case", async () => {
+  let useCase: LoginUseCase;
+  let userRepository: InMemoryUserRepository;
+  const userEmail = "john.doe@example.com";
+  const userPassword = "Password123!";
+  const user = User.from(
+    Email.from(userEmail),
+    await Password.from(userPassword)
+  );
+
+  beforeAll(() => {
+    userRepository = testContainer.get<InMemoryUserRepository>(
+      SYMBOLS.IReadUserRepository
+    );
+    useCase = testContainer.get<LoginUseCase>(SYMBOLS.LoginUseCase);
+  });
+
+  beforeEach(async () => {
+    await userRepository.save(user);
+  });
+
+  afterEach(() => {
+    userRepository.clear();
+  });
+
+  test("Should login successfully with correct credentials", async () => {
+    const input = {
+      email: userEmail,
+      password: userPassword,
+    };
+    const output = await useCase.execute(input);
+    expect(output).toBeDefined();
+    expect(output.userId).toBe(user.id);
+    expect(output.token).toMatch(JWT_REGEX);
+  });
+});
