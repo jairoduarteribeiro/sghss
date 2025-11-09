e7b39f774b6fb5924fdffe6ad028b7e07970b120
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-11-09 10:45:31 +0000

    refactor: mock repository in RegisterPatient

--- src/application/use-cases/register-patient.use-case.ts
+++ src/application/use-cases/register-patient.use-case.ts
@@ -4,7 +4,11 @@ import { Name } from "@/domain/value-objects/name";
 import { Uuid } from "@/domain/value-objects/uuid";
 import { SYMBOLS } from "@/inversify.symbols";
 import { inject, injectable } from "inversify";
-import type { IWritePatientRepository } from "../repositories/patient.repository";
+import type {
+  IReadPatientRepository,
+  IWritePatientRepository,
+} from "../repositories/patient.repository";
+import { ConflictError } from "../errors/conflict.error";
 
 type RegisterPatientInput = {
   name: string;
@@ -22,10 +26,17 @@ type RegisterPatientOutput = {
 @injectable()
 export class RegisterPatientUseCase {
   constructor(
+    @inject(SYMBOLS.IReadPatientRepository)
+    private readPatientRepository: IReadPatientRepository,
     @inject(SYMBOLS.IWritePatientRepository)
     private writePatientRepository: IWritePatientRepository
   ) {}
+
   async execute(input: RegisterPatientInput): Promise<RegisterPatientOutput> {
+    const cpf = Cpf.from(input.cpf);
+    if (await this.cpfExists(cpf)) {
+      throw new ConflictError("CPF already in use");
+    }
     const patient = Patient.from(
       Name.from(input.name),
       Cpf.from(input.cpf),
@@ -38,5 +49,10 @@ export class RegisterPatientUseCase {
       cpf: patient.cpf,
       userId: patient.userId,
     };
+  }
+
+  private async cpfExists(cpf: Cpf): Promise<boolean> {
+    const existingPatient = await this.readPatientRepository.findByCpf(cpf);
+    return existingPatient !== null;
   }
 }
--- tests/application/use-cases/register-patient.use-case.test.ts
+++ tests/application/use-cases/register-patient.use-case.test.ts
@@ -1,50 +1,89 @@
-import type { IWritePatientRepository } from "@/application/repositories/patient.repository";
-import type { IWriteUserRepository } from "@/application/repositories/user.repository";
+import type {
+  IReadPatientRepository,
+  IWritePatientRepository,
+} from "@/application/repositories/patient.repository";
 import type { RegisterPatientUseCase } from "@/application/use-cases/register-patient.use-case";
 import { container } from "@/config/inversify.container";
-import { User } from "@/domain/entities/user";
-import { Email } from "@/domain/value-objects/email";
-import { Password } from "@/domain/value-objects/password";
+import { Patient } from "@/domain/entities/patient";
+import { Cpf } from "@/domain/value-objects/cpf";
+import { Uuid } from "@/domain/value-objects/uuid";
+import { Name } from "@/domain/value-objects/name";
 import { SYMBOLS } from "@/inversify.symbols";
-import { describe, test, expect, afterEach, beforeAll } from "bun:test";
+import {
+  describe,
+  test,
+  expect,
+  afterEach,
+  mock,
+  afterAll,
+  beforeAll,
+} from "bun:test";
 
-describe("Register Patient Use Case", () => {
+describe("Register Patient Use Case", async () => {
   let useCase: RegisterPatientUseCase;
-  let writeUserRepository: IWriteUserRepository;
-  let writePatientRepository: IWritePatientRepository;
+  const existingPatient = Patient.from(
+    Name.from("John Doe"),
+    Cpf.from("70000000400"),
+    Uuid.generate()
+  );
+  const mockReadPatientRepository: IReadPatientRepository = {
+    findByCpf: mock(async (cpf: Cpf) =>
+      cpf.value === existingPatient.cpf ? existingPatient : null
+    ),
+  };
+  const mockWritePatientRepository: IWritePatientRepository = {
+    save: mock(async (patient: Patient) => {}),
+    clear: mock(async () => {}),
+  };
 
-  beforeAll(() => {
+  beforeAll(async () => {
+    container.snapshot();
+    (
+      await container.rebind<IReadPatientRepository>(
+        SYMBOLS.IReadPatientRepository
+      )
+    ).toConstantValue(mockReadPatientRepository);
+    (
+      await container.rebind<IWritePatientRepository>(
+        SYMBOLS.IWritePatientRepository
+      )
+    ).toConstantValue(mockWritePatientRepository);
     useCase = container.get<RegisterPatientUseCase>(
       SYMBOLS.RegisterPatientUseCase
-    );
-    writeUserRepository = container.get<IWriteUserRepository>(
-      SYMBOLS.IWriteUserRepository
-    );
-    writePatientRepository = container.get<IWritePatientRepository>(
-      SYMBOLS.IWritePatientRepository
     );
   });
 
   afterEach(() => {
-    writePatientRepository.clear();
-    writeUserRepository.clear();
+    mock.clearAllMocks();
+  });
+
+  afterAll(() => {
+    container.restore();
   });
 
   test("Should register a patient successfully", async () => {
-    const user = User.from(
-      Email.from("john.doe@example.com"),
-      await Password.from("Password123!")
-    );
-    await writeUserRepository.save(user);
-    const output = await useCase.execute({
-      name: "John Doe",
-      cpf: "70000000400",
-      userId: user.id,
-    });
+    const userId = Uuid.generate();
+    const input = {
+      name: "Jane Doe",
+      cpf: "12984180038",
+      userId: userId.value,
+    };
+    const output = await useCase.execute(input);
+    expect(mockWritePatientRepository.save).toHaveBeenCalledTimes(1);
     expect(output).toBeDefined();
     expect(output.patientId).toBeDefined();
-    expect(output.name).toBe("John Doe");
-    expect(output.cpf).toBe("70000000400");
-    expect(output.userId).toBe(user.id);
+    expect(output.name).toBe("Jane Doe");
+    expect(output.cpf).toBe("12984180038");
+    expect(output.userId).toBe(userId.value);
+  });
+
+  test("Should not register a patient with an existing Cpf", async () => {
+    const input = {
+      name: "John Smith",
+      cpf: "70000000400",
+      userId: Uuid.generate().value,
+    };
+    expect(useCase.execute(input)).rejects.toThrowError("CPF already in use");
+    expect(mockWritePatientRepository.save).toHaveBeenCalledTimes(0);
   });
 });
