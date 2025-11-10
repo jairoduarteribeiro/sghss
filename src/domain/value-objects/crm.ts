61c4f0999a3fe74f77f7b39bcad313f6c02bd4b4
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-11-10 10:51:08 +0000

    feat: should not create a Crm with invalid state

--- src/domain/value-objects/crm.ts
+++ src/domain/value-objects/crm.ts
@@ -1,6 +1,35 @@
 import { ValidationError } from "../errors/validation.error";
 
 const CRM_REGEX = /^\d{1,6}-[A-Z]{2}$/;
+const VALID_STATES = [
+  "AC",
+  "AL",
+  "AP",
+  "AM",
+  "BA",
+  "CE",
+  "DF",
+  "ES",
+  "GO",
+  "MA",
+  "MT",
+  "MS",
+  "MG",
+  "PA",
+  "PB",
+  "PR",
+  "PE",
+  "PI",
+  "RJ",
+  "RN",
+  "RS",
+  "RO",
+  "RR",
+  "SC",
+  "SP",
+  "SE",
+  "TO",
+];
 
 export class Crm {
   private constructor(readonly value: string) {}
@@ -14,9 +43,21 @@ export class Crm {
     if (!this.hasValidFormat(crm)) {
       throw new ValidationError("CRM with invalid format");
     }
+    const state = this.extractState(crm);
+    if (!this.isValidState(state)) {
+      throw new ValidationError("CRM with invalid state");
+    }
   }
 
   private static hasValidFormat(crm: string): boolean {
     return CRM_REGEX.test(crm);
+  }
+
+  private static extractState(crm: string): string {
+    return crm.slice(-2);
+  }
+
+  private static isValidState(state: string): boolean {
+    return VALID_STATES.includes(state);
   }
 }
