32f66517805455167474cbef6e2abd324dc8de7d
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-11-10 16:22:02 +0000

    feat: should not register a Doctor with existing Crm

--- src/application/use-cases/register-doctor.ts
+++ src/application/use-cases/register-doctor.ts
@@ -1,10 +1,14 @@
 import { inject, injectable } from "inversify";
 import { SYMBOLS } from "../di/inversify.symbols";
-import type { IWriteDoctorRepository } from "../ports/repositories/doctor.repository";
+import type {
+  IReadDoctorRepository,
+  IWriteDoctorRepository,
+} from "../ports/repositories/doctor.repository";
 import { Doctor } from "../../domain/entities/doctor";
 import { Name } from "../../domain/value-objects/name";
 import { Crm } from "../../domain/value-objects/crm";
 import { Uuid } from "../../domain/value-objects/uuid";
+import { ConflictError } from "../errors/conflict.error";
 
 type RegisterDoctorInput = {
   name: string;
@@ -22,13 +26,20 @@ type RegisterDoctorOutput = {
 @injectable()
 export class RegisterDoctorUseCase {
   constructor(
+    @inject(SYMBOLS.IReadDoctorRepository)
+    private readonly readDoctorRepository: IReadDoctorRepository,
     @inject(SYMBOLS.IWriteDoctorRepository)
     private readonly writeDoctorRepository: IWriteDoctorRepository
   ) {}
+
   async execute(input: RegisterDoctorInput): Promise<RegisterDoctorOutput> {
+    const crm = Crm.from(input.crm);
+    if (await this.crmExists(crm)) {
+      throw new ConflictError("Crm already in use");
+    }
     const doctor = Doctor.from(
       Name.from(input.name),
-      Crm.from(input.crm),
+      crm,
       Uuid.fromString(input.userId)
     );
     await this.writeDoctorRepository.save(doctor);
@@ -38,5 +49,10 @@ export class RegisterDoctorUseCase {
       crm: doctor.crm,
       userId: doctor.userId,
     };
+  }
+
+  private async crmExists(crm: Crm): Promise<boolean> {
+    const existingDoctor = await this.readDoctorRepository.findByCrm(crm);
+    return existingDoctor !== null;
   }
 }
