7d8d3521d3295def2bc209251f743ac6d04819ed
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-11-10 10:37:36 +0000

    feat: should not create a Crm with invalid format

--- src/domain/value-objects/crm.ts
+++ src/domain/value-objects/crm.ts
@@ -1,7 +1,22 @@
+import { ValidationError } from "../errors/validation.error";
+
+const CRM_REGEX = /^\d{1,6}-[A-Z]{2}$/;
+
 export class Crm {
   private constructor(readonly value: string) {}
 
   static from(crm: string): Crm {
+    this.validate(crm);
     return new Crm(crm);
+  }
+
+  private static validate(crm: string): void {
+    if (!this.hasValidFormat(crm)) {
+      throw new ValidationError("CRM with invalid format");
+    }
+  }
+
+  private static hasValidFormat(crm: string): boolean {
+    return CRM_REGEX.test(crm);
   }
 }
