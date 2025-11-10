6b747dca59eb805a0a2f30b1d3c38d9fafca24ca
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-11-10 13:31:36 +0000

    chore: configure coverage report

--- src/application/services/auth-token-generator.ts
+++ src/application/services/auth-token-generator.ts
@@ -13,10 +13,6 @@ export interface IAuthTokenGenerator {
 @injectable()
 export class JwtTokenGenerator implements IAuthTokenGenerator {
   generate(payload: AuthTokenPayload): string {
-    if (!process.env.JWT_SECRET) {
-      throw new Error("JWT_SECRET is not defined in environment variables");
-    }
-    const secret = process.env.JWT_SECRET;
-    return jwt.sign(payload, secret, { expiresIn: "1h" });
+    return jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: "1h" });
   }
 }
