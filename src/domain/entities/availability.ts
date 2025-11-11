22891a2cb0cc728009900780d74ddf189ae93547
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2025-11-11 18:38:33 +0000

    feat: should create an Availability

---
+++ src/domain/entities/availability.ts
@@ -0,0 +1,48 @@
+import { Uuid } from "../value-objects/uuid";
+
+export class Availability {
+  private constructor(
+    private readonly _id: Uuid,
+    private readonly _startDateTime: Date,
+    private readonly _endDateTime: Date,
+    private readonly _doctorId: Uuid,
+  ) {}
+
+  static from(
+    startDateTime: Date,
+    endDateTime: Date,
+    doctorId: Uuid,
+  ): Availability {
+    return new Availability(
+      Uuid.generate(),
+      startDateTime,
+      endDateTime,
+      doctorId,
+    );
+  }
+
+  static restore(
+    id: Uuid,
+    startDateTime: Date,
+    endDateTime: Date,
+    doctorId: Uuid,
+  ): Availability {
+    return new Availability(id, startDateTime, endDateTime, doctorId);
+  }
+
+  get id(): string {
+    return this._id.value;
+  }
+
+  get startDateTime(): Date {
+    return this._startDateTime;
+  }
+
+  get endDateTime(): Date {
+    return this._endDateTime;
+  }
+
+  get doctorId(): string {
+    return this._doctorId.value;
+  }
+}
